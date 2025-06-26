a288f4258c5081b5d36ba948f6010fe5faa2f21b
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-06-26 11:00:09 -0700

    tw-load-scratch-blocks-hoc.jsx -- unused log

--- src/lib/tw-load-scratch-blocks-hoc.jsx
+++ src/lib/tw-load-scratch-blocks-hoc.jsx
@@ -12,7 +12,7 @@ const LoadScratchBlocksHOC = function (WrappedComponent) {
                 loaded: LazyScratchBlocks.isLoaded(),
                 error: null
             };
-            console.log(this.state.loaded);
+            //console.log(this.state.loaded);
             if (!this.state.loaded) {
                 LazyScratchBlocks.load()
                     .then(() => {
